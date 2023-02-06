d26fe6331e7a2aafabcbfff0bd59b519ae9d82ae
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-06 14:03:49 +0000

    first commit before creating classes.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// create variable for api key
+// create variable for url api call.
+// variable for geocoding api call if diff..
+// variable for current/daily forcast div 
+// create variable for 5 day forcast single time of day for each 
+// create bootstrap buttons div
+// create variable to create city search buttons dynamically    
+
+const APIKey = 20022a5186516909b5b3aa143967e8fe;
